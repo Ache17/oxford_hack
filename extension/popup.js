52817f8a06d3aca900e5e88a97f41c37c7506f04
Alex Constantin <alex.constantin432@gmail.com>
2020-11-14 16:14:03 +0000

    added httprequest

--- extension/popup.js
+++ extension/popup.js
@@ -12,8 +12,16 @@ document.addEventListener('DOMContentLoaded', function() {
           var tab = array_of_Tabs[0];
           console.log("submitMHTML() found the active tab has an ID of " + tab.id);
           chrome.pageCapture.saveAsMHTML({tabId: tab.id}, function(mhtml) {
-              //const html = mhtml2html.convert(mhtml);
-              console.log(mhtml.text())
+              var xhr = new XMLHttpRequest();
+              var textMhtml = JSON.stringify(mhtml.text());
+              console.log(typeof(textMhtml));
+              xhr.open("POST", "http://20.39.216.243/api/caption");
+              xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+              xhr.setRequestHeader("Accept", "application/json");
+              xhr.onreadystatechange = function(){
+                console.log(this.responseText);
+              }
+              xhr.send(JSON.stringify({"mhtml":textMhtml}));
               console.log("submitMHTML() sent mhtml to server");
             }
           )
