db4faaac3ac5fa437af2ac4edaee65e9aaa9b2c9
Maciej <maciej.lewandowski@student.manchester.ac.uk>
2020-11-14 11:30:48 +0000

    mock api

---
+++ backend/main.py
@@ -0,0 +1,58 @@
+from flask import Flask
+from flask_restful import Resource, Api, reqparse
+from random import choice
+
+
+ID_size = 32
+
+app = Flask(__name__)
+api = Api(app)
+parser = reqparse.RequestParser()
+
+imageCaptionPOSTParser = reqparse.RequestParser()
+imageCaptionPOSTParser.add_argument("website", type=list, help="please include 'website' with source of the html to caption", required=True)
+
+imageCaptionGETParser = reqparse.RequestParser()
+imageCaptionGETParser.add_argument("ID", type=list, help="include ID that were give to you while submitting the job" , required=True)
+
+
+def getRandomID():
+    return "".join([choice("0123456789") for _ in range(ID_size)])
+
+def get_parsed_html():
+    return "here goes the parsed html website"
+
+def submit_to_calculation():
+    ID = getRandomID()
+    return ID 
+
+
+class imageCaptionTest(Resource):
+    def get(self):
+        return {"hello" : "world"}
+
+    def post(self):
+        args = parser.parse_args()
+        return {"message" : "ok fine", "test" : "test"}, 201
+
+
+class imageCaption(Resource):
+    def get(self):
+        args = imageCaptionGETParser.parser_args()
+        return {"html" : get_parsed_html()}
+
+    def post(self):    
+        args = imageCaptionPOSTParser.parse_args()
+        print(args.website)
+
+        # submit to some queing system 
+        ID = submit_to_calculation()
+
+        return {"message" : "Sucessfully uploaded task to API", "ID" : ID}, 201
+
+
+api.add_resource(imageCaptionTest, "/")
+api.add_resource(imageCaption, "/api/caption")
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- extension/popup.js
+++ extension/popup.js
@@ -6,11 +6,17 @@ chrome.storage.sync.get('color', function(data) {
   });
 
 
+var run_translation = 'console.log("xd");';
+
+
 changeColor.onclick = function(element) {
   let color = element.target.value;
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     chrome.tabs.executeScript(
         tabs[0].id,
-        {code: 'document.body.style.backgroundColor = "' + color + '";'});
+        {code: run_translation});
   });
 };
+
+
+
