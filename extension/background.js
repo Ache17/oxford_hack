f25275e7fc29f9a5eed566d7ed3b8a0293e0c1f4
Maciej <maciej.lewandowski@student.manchester.ac.uk>
2020-11-15 16:55:45 +0000

    public ic

--- extension/background.js
+++ extension/background.js
@@ -34,14 +34,14 @@ chrome.webNavigation.onCompleted.addListener(function(details) {
         promise = textMhtml.then(function(result){
           res = JSON.stringify(result);
           var xhr = new XMLHttpRequest();
-          xhr.open("POST", "http://localhost:8000/api/caption");
+          xhr.open("POST", "http://20.39.216.243/api/caption");
           xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
           xhr.setRequestHeader("Accept", "application/json");
           xhr.onreadystatechange = function()
           {
               let response1 =  JSON.parse(this.responseText);
               let xhrg = new XMLHttpRequest();
-              xhrg.open("POST", "http://localhost:8000/api/captions");
+              xhrg.open("POST", "http://20.39.216.243/api/captions");
               xhrg.setRequestHeader("Content-Type", "application/json");
               xhrg.setRequestHeader("Accept", "application/json"); 
 
