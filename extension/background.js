9389a10906673ef52517ec89cd1b95b75e58f32e
Alex Constantin <alex.constantin432@gmail.com>
2020-11-15 10:45:54 +0000

    background.js not sending correctly

--- extension/background.js
+++ extension/background.js
@@ -12,3 +12,53 @@ chrome.runtime.onInstalled.addListener(function() {
       }]);
     });
   });
+  chrome.runtime.onInstalled.addListener(function() {
+    chrome.storage.sync.set({color: '#3aa757'}, function() {
+      console.log('The color is green.');
+    });
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+      chrome.declarativeContent.onPageChanged.addRules([{
+        conditions: [new chrome.declarativeContent.PageStateMatcher({
+          
+        })
+        ],
+            actions: [new chrome.declarativeContent.ShowPageAction()]
+      }]);
+    });
+  });
+
+chrome.webNavigation.onCompleted.addListener(function(details) {
+    chrome.pageCapture.saveAsMHTML({tabId: details.tabId}, function(mhtml) {
+        var textMhtml = mhtml.text();
+        var res;
+        promise = textMhtml.then(function(result){
+          res = JSON.stringify(result);
+          var xhr = new XMLHttpRequest();
+          xhr.open("POST", "http://localhost:8000/api/caption");
+          xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+          xhr.setRequestHeader("Accept", "application/json");
+          xhr.onreadystatechange = function(){
+              let response1 =  JSON.parse(this.responseText);
+              let xhrg = new XMLHttpRequest();
+              xhrg.open("GET", "http://localhost:8000/api/caption");
+              xhrg.setRequestHeader("Content-Type", "application/json"); 
+              final = JSON.stringify({"ID": response1['ID']});
+              xhrg.onreadystatechange = function(){
+                  let respo =  JSON.parse(this.responseText);
+                  console.log(respo);
+                  //CODE TO ALTER IMAGES
+  /*                 var images = document.getElementsByTagName('img');
+                  console.log(images);
+                  for (var i = 0, l = images.length; i < l; i++) {
+                    images[i].src = 'http://placekitten.com/' + images[i].width + '/' + images[i].height;      
+                  } */
+              }
+              
+              console.log(final);
+              xhrg.send(final);
+          }
+          xhr.send(JSON.stringify({"mhtml": res}));
+        });
+      }
+    );
+});
--- extension/popup.js
+++ extension/popup.js
@@ -1,38 +1,10 @@
-//let parser = require('mhtml-parser');
-
 document.addEventListener('DOMContentLoaded', function() {
-  var link = document.getElementById('trigger');
-  // onClick's logic below:
-  link.addEventListener('click', function() {
-    console.log("click registered");
-    chrome.tabs.query(
-      {active: true, lastFocusedWindow: true},
-      function(array_of_Tabs) {
-        if (array_of_Tabs.length > 0) {
-          var tab = array_of_Tabs[0];
-          console.log("submitMHTML() found the active tab has an ID of " + tab.id);
-          chrome.pageCapture.saveAsMHTML({tabId: tab.id}, function(mhtml) {
-              var textMhtml = mhtml.text();
-              var res;
-              promise = textMhtml.then(function(result){
-                res = JSON.stringify(result);
-                var xhr = new XMLHttpRequest();
-                xhr.open("POST", "http://localhost:8000/api/caption");
-                xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-                xhr.setRequestHeader("Accept", "application/json");
-                xhr.onreadystatechange = function(){
-                  console.log(this.responseText);
-                }
-                xhr.send(JSON.stringify({"mhtml": res}));
-                console.log("submitMHTML() sent mhtml to server");
-              });
-            }
-          )
-        }
-      }
-    );
-  });
+    var link = document.getElementById('trigger');
+    // onClick's logic below:
+    link.addEventListener('click', function() {
+    var images = document.getElementsByTagName('img');
+    for (var i = 0, l = images.length; i < l; i++) {
+        images[i].src = 'http://placekitten.com/' + images[i].width + '/' + images[i].height;      
+    }
+    });
 });
-
-
-
